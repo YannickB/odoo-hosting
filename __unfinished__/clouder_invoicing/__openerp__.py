ec398ad64fb13fa16fad242bffe48827946b8c5f
nicolas-petit <nicolas.petit@vivre-d-internet.fr>
2016-05-01 05:36:51 +0200

    Added descriptions to invoicing plugins

--- __unfinished__/clouder_invoicing/__openerp__.py
+++ __unfinished__/clouder_invoicing/__openerp__.py
@@ -29,7 +29,7 @@
     'license': 'Other OSI approved licence',
     'website': 'https://github.com/clouder-community/clouder',
     'description': """
-    Clouder
+    Adds invoicing capabilities to clouder container/bases
     """,
     'demo': [],
     'data': [
--- __unfinished__/clouder_invoicing_master/__openerp__.py
+++ __unfinished__/clouder_invoicing_master/__openerp__.py
@@ -29,7 +29,7 @@
     'license': 'Other OSI approved licence',
     'website': 'https://github.com/clouder-community/clouder',
     'description': """
-    Clouder
+    Pilots sub-clouder instances and invoicing
     """,
     'demo': [],
     'data': [
