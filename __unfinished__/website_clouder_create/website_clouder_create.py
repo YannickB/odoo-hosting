d6f2ac56eae3f8087fe0f915e3cd8dead3dc51ac
nicolas-petit <nicolas.petit@vivre-d-internet.fr>
2016-05-11 20:57:03 +0200

    Implemented code and view to make partner creation possible.
    The code is ported from the website_sale plugin, minus some functionnality
    that do not interest us at the moment.

--- __unfinished__/website_clouder_create/website_clouder_create.py
+++ __unfinished__/website_clouder_create/website_clouder_create.py
@@ -20,8 +20,7 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api, http, _
-from openerp.exceptions import except_orm
+from openerp import models, fields, api, http, SUPERUSER_ID _
 from openerp.http import request
 
 import logging
@@ -47,28 +46,65 @@ class ClouderContainer(models.Model):
         return 0
 
 
+class ClouderApplication(models.Model):
+    """
+    Adds information for web-creation on applications
+    """
+
+    _inherit = 'clouder.application'
+
+    web_create_type = fields.Selection(
+        [
+            ('disabled', 'Disabled'),
+            ('container', 'Container'),
+            ('base', 'Base')
+        ],
+        'Web creation',
+        default='disabled'
+    )
+
+
 class WebsiteClouderCreate(http.Controller):
     """
     Defines a webpage to request creation of clouder container/bases
     """
 
-    def create_form_validate(self, vals):
+    partner_mandatory_fields = [
+        "name",
+        "phone",
+        "email",
+        "street2",
+        "city",
+        "country_id",
+    ]
+
+    other_mandatory_fields = [
+        'application_id'
+    ]
+
+    partner_optionnal_fields = [
+        "street",
+        "zip",
+        "state_id"
+    ]
+
+    def create_form_validate(self, data):
         """
         Checks that the necessary values are filled correctly
         """
-        # TODO: implement
-        vals['error'] = {}
-        return vals
+        # Validation
+        error = dict()
+        for field_name in self.partner_mandatory_fields + self.other_mandatory_fields:
+            if not data.get(field_name):
+                error[field_name] = 'missing'
+        return error
 
     @http.route(['/instance/new'], type='http', auth="public", website=True)
     def display_new_form(self, **post):
         """
         Displays the web form to create a new instance
         """
-        values = {
-            'error': {},
-            'form_data': {}
-        }
+        values = self.form_values()
 
         return request.render("website_clouder_create.create_form", values)
 
@@ -78,11 +114,117 @@ class WebsiteClouderCreate(http.Controller):
         Validates data and launches the instance creation process
         """
         # Check that the form is correct
-        values = self.create_form_validate(post)
+        values = self.form_values(post)
+        values['error'] = self.create_form_validate(values['form_data'])
         # Return to the first form on error
-        if 'error' in values and values['error']:
+        if values['error']:
             return request.render("website_clouder_create.create_form", values)
 
-        res = request.env['clouder.container'].create_instance(values)
+        # Create partner
+        values['partner_id'] = self.instance_form_save(values['form_data'])
+
+        res = request.env['clouder.container'].create_instance({
+            # TODO: fill in required vals
+        })
 
         return request.render("website_clouder_create.create_validation", {'res': res})
+
+    def instance_form_save(self, checkout):
+        cr, uid, context, registry = request.cr, request.uid, request.context, request.registry
+
+        orm_partner = registry.get('res.partner')
+        orm_user = registry.get('res.users')
+
+        partner_lang = request.lang if request.lang in [lang.code for lang in request.website.language_ids] else None
+
+        partner_info = {'customer': True}
+        if partner_lang:
+            partner_info['lang'] = partner_lang
+        partner_info.update(self.parse_partner_fields(checkout))
+        del partner_info['application_id']
+
+        # set partner_id
+        partner_id = None
+        if uid != request.website.user_id.id:
+            partner_id = orm_user.browse(cr, SUPERUSER_ID, uid, context=context).partner_id.id
+
+        # save partner informations
+        if partner_id and request.website.partner_id.id != partner_id:
+            orm_partner.write(cr, SUPERUSER_ID, [partner_id], partner_info, context=context)
+        else:
+            # create partner
+            partner_id = orm_partner.create(cr, SUPERUSER_ID, partner_info, context=context)
+
+        if not isinstance(partner_id, int):
+            partner_id = partner_id.id
+
+        return partner_id
+
+    def parse_partner_fields(self, data):
+        # set mandatory and optional fields
+        all_fields = self.partner_mandatory_fields + \
+                     self.partner_optionnal_fields + \
+                     self.other_mandatory_fields
+
+        # set data
+        if isinstance(data, dict):
+            query = dict((field_name, data[field_name])
+                for field_name in all_fields if field_name in data)
+        else:
+            query = dict((field_name, getattr(data, field_name))
+                for field_name in all_fields if getattr(data, field_name))
+            if data.parent_id:
+                query['street'] = data.parent_id.name
+
+        if query.get('state_id'):
+            query['state_id'] = int(query['state_id'])
+        if query.get('country_id'):
+            query['country_id'] = int(query['country_id'])
+        if query.get('application_id'):
+            query['application_id'] = int(query['application_id'])
+
+        return query
+
+    def form_values(self, data=None):
+        """
+        Parses the values used in the form
+        If data is not provided, creates default values for the form
+        """
+        cr, uid, context, registry = request.cr, request.uid, request.context, request.registry
+        orm_user = registry.get('res.users')
+        orm_country = registry.get('res.country')
+        state_orm = registry.get('res.country.state')
+
+        country_ids = orm_country.search(cr, SUPERUSER_ID, [], context=context)
+        countries = orm_country.browse(cr, SUPERUSER_ID, country_ids, context)
+        states_ids = state_orm.search(cr, SUPERUSER_ID, [], context=context)
+        states = state_orm.browse(cr, SUPERUSER_ID, states_ids, context)
+        partner = orm_user.browse(cr, SUPERUSER_ID, request.uid, context).partner_id
+        applications = request.env['clouder.application'].search(
+            [('web_create_type', '!=', 'disabled')]
+        )
+
+        form_data = {}
+        if not data:
+            if request.uid != request.website.user_id.id:
+                form_data.update( self.parse_partner_fields(partner) )
+        else:
+            form_data = self.parse_partner_fields(data)
+
+        # Default search by user country
+        if not form_data.get('country_id'):
+            country_code = request.session['geoip'].get('country_code')
+            if country_code:
+                country_ids = request.registry.get('res.country').search(cr, uid, [('code', '=', country_code)], context=context)
+                if country_ids:
+                    form_data['country_id'] = country_ids[0]
+
+        values = {
+            'countries': countries,
+            'states': states,
+            'form_data': form_data,
+            'applications': applications,
+            'error': {}
+        }
+
+        return values
