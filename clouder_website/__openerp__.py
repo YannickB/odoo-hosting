b5a8a9ae0f0cd81489b6fd5dd0dd1ba36a1a6bf9
nicolas-petit <nicolas.petit@vivre-d-internet.fr>
2016-09-01 18:08:15 +0200

    Changed CSS of the form completely. Made sure the new CSS would only affect the ClouderPlugin div when integrated inside a page. Please not that this cannot make sure the page's CSS cannot override some styles.
    Added fontawesome support in.
    Added mention of adding fontawesome in __openerp__.
    
    The new CSS has been tested both integrated in an empty page and inside
    an Odoo website page.

--- clouder_website/__openerp__.py
+++ clouder_website/__openerp__.py
@@ -30,6 +30,8 @@
     'website': 'https://github.com/clouder-community/clouder',
     'description': """
     Creates an HTTP controller that serves a form to create new clouder instances from an external website
+
+    Uses Font Awesome by Dave Gandy - http://fontawesome.io
     """,
     'demo': [],
     'data': [
